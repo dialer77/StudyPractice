ee4364eb172ddc19c24d6b7d7966590f1d8cdca8
KY_Kwon <user@DESKTOP-K1FJKSG>
2025-01-09 15:55:41 +0900

    예시 문제 작성

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("GJ_Ryu");
+        Console.WriteLine("No1 - https://www.acmicpc.net/problem/1085");
+        
+        int[] inputs = new int[] { 6, 2, 10, 3 };
+        GJ_Ryu.No1 no1 = new GJ_Ryu.No1(inputs);
 
+        Console.WriteLine($"input: {String.Join(", ", inputs)}");
+        Console.WriteLine("output: " + no1.Solve().ToString());
 
-Console.WriteLine("Hello, Study3");
+    }
+}
---
+++ personalFolder/GJ_Ryu/No1.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO.Pipelines;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GJ_Ryu
+{
+    // https://www.acmicpc.net/problem/1085
+    public class No1
+    {
+        private int[] m_inputs = null;
+        public No1(int[] inputs) 
+        { 
+            m_inputs = inputs;
+        }
+
+
+        public int Solve()
+        {
+            int result = 0;
+            // 문제 확인후 여기 풀이 작성
+
+            return result;
+        }
+            
+    }
+}
