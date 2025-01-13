ee58c1ac5cb32f3be380e51de014421bb6484fab
kwon.ky <smm1@naver.com>
2025-01-13 23:15:29 +0900

    Review: No1002 변수명 풀네임작성 중간 계산과정 단게별 저장 계산 누락 조건 추가
    내용을 확인했을때 로직 설명없이도 코드가 이해되기 쉬운 형태로 작성하는게 좋은 방향성입니다
    변수명을 굳이 약어로 사용하였을시 얻는 장점은 거의 없고 추후 다른사람 또는 자신이 볼때 이해하기 어려울수 있습니다

--- personalFolder/HS_Lee/Review/No1002.cs
+++ personalFolder/HS_Lee/Review/No1002.cs
@@ -1,31 +1,93 @@
+using System.Reflection;
+
 namespace HS_Lee.Review
 {
     public class No1002
     {
-        //https://www.acmicpc.net/problem/1002
-        private int[] m_inputs = null;
-        public No1002(int[] inputs)
-        {
-            m_inputs = inputs;
-        }
+        // public static void Main()
+        // {
+        //     string strTestCount = Console.ReadLine()!;
+        //     int testCount = int.Parse(strTestCount);
+        //     int[] testResult = new int[testCount];
+        //     No1002 no1002 = new No1002();
+        //     for (int i = 0; i < testCount; i++)
+        //     {
+        //         string inputLine = Console.ReadLine()!;
+        //         string[] strInputs = inputLine.Split(' ');
+        //         int[] inputs = new int[strInputs.Length];
+        //         for (int j = 0; j < strInputs.Length; j++)
+        //         {
+        //             inputs[j] = int.Parse(strInputs[j]);
+        //         }
 
-        public int Solve()
+        //         testResult[i] = no1002.Solve(inputs);
+
+        //     }
+
+        //     for (int i = 0; i < testResult.Length; i++)
+        //     {
+        //         Console.WriteLine(testResult[i]);
+        //     }
+        // }
+
+        //https://www.acmicpc.net/problem/1002
+
+        public int Solve(int[] inputs)
         {
             int result = 0;
-            double b, h, c; //base, Height, Hypotenuse
+            int pointX1 = inputs[0];
+            int pointY1 = inputs[1];
+            int radius1 = inputs[2];
 
-            b = Math.Abs(m_inputs[0] - m_inputs[3]);
-            h = Math.Abs(m_inputs[1] - m_inputs[4]);
-            c = Math.Sqrt(Math.Pow(b, 2) + Math.Pow(h, 2));
+            int pointX2 = inputs[3];
+            int pointY2 = inputs[4];
+            int radius2 = inputs[5];
 
-            if (m_inputs[2] + m_inputs[5] == c)
-                result = 1;
-            else if (m_inputs[2] + m_inputs[5] > c && Math.Abs(m_inputs[2] - m_inputs[5]) < c)
-                result = 2;
-            else if (m_inputs[2] + m_inputs[5] < c || Math.Abs(m_inputs[2] - m_inputs[5]) > c)
+            double pointDistance = Math.Pow(pointX1 - pointX2, 2) + Math.Pow(pointY1 - pointY2, 2);
+            double radiusSum = Math.Pow(radius1 + radius2, 2);
+            double radiusSub = Math.Pow(radius2 - radius1, 2);
+
+            // 점의 위치가 같을경우
+            if (pointDistance == 0)
+            {
+                // 거리가 같으면 원의 둘레전체 무한대의 좌표
+                if (radius1 == radius2)
+                {
+                    result = -1;
+                }
+                // 거리가 다르면 겹치는 좌표가 없음
+                else
+                {
+                    result = 0;
+                }
+
+            }
+            // 큰원안에 작은 원이 들어갈때
+            else if (pointDistance < radiusSub)
+            {
                 result = 0;
-            else
-                result = -1;
+
+            }
+            // 큰원안에 작은원이 접할때
+            else if (pointDistance == radiusSub)
+            {
+                result = 1;
+            }
+            // 두개의 원이 떨어져있을때
+            else if (pointDistance > radiusSum)
+            {
+                result = 0;
+            }
+            // 두개의 원이 밖에서 접할때
+            else if (pointDistance == radiusSum)
+            {
+                result = 1;
+            }
+            // 두개의 원이 겹칠때
+            else if (pointDistance < radiusSum)
+            {
+                result = 2;
+            }
 
             return result;
         }
