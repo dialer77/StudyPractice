6a2b79d33b293e8e687ed3f7136eb1c12193a55b
LHS0511 <lhss0511@gmail.com>
2025-01-13 09:07:54 +0900

    ADD N0.1002

--- Program.cs
+++ Program.cs
@@ -12,9 +12,16 @@
         Console.WriteLine("output: " + no1.Solve().ToString());
 
         //No. 1000
+        inputs = new int[] { 1, 2 };
         HS_Lee.No1000 no1000 = new HS_Lee.No1000(inputs);
         Console.WriteLine($"input: {String.Join(", ", inputs)}");
         Console.WriteLine("output: " + no1000.Solve().ToString());
+
+        //NO. 1002
+        inputs = new int[] { 0, 0, 13, 40, 0, 37 };
+        HS_Lee.No1002 no1002 = new HS_Lee.No1002(inputs);
+        Console.WriteLine($"input: {String.Join(", ", inputs)}");
+        Console.WriteLine("output: " + no1002.Solve().ToString());
 
     }
 }
