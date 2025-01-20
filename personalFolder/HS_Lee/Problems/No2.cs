2e473ffca95d95ef89dea05672d92706c0155ecc
LHS0511 <lhss0511@gmail.com>
2025-01-20 15:56:26 +0900

    문제풀이 추가

--- personalFolder/HS_Lee/Problems/No2.cs
+++ personalFolder/HS_Lee/Problems/No2.cs
@@ -8,6 +8,26 @@ namespace HS_Lee.Problems
         public int solution(string s)
         {
             int answer = 0;
+            int nCnt1 = 0;
+            int nCnt2 = 0;
+
+            char FirstChar = s[0];
+            
+            while(s.Length > 0)
+            {
+                s.Substring(1);
+                if(FirstChar == s[0])
+                    nCnt1++;
+                else
+                    nCnt2++;
+
+                if(nCnt1 == nCnt2)
+                {
+                    answer++;
+                    nCnt1 = nCnt2 = 0;
+                }                
+            }
+
             return answer;
         }
     }
--- personalFolder/HS_Lee/Problems/No3.cs
+++ personalFolder/HS_Lee/Problems/No3.cs
@@ -8,6 +8,26 @@ namespace HS_Lee.Problems
         public int solution(int n)
         {
             int answer = 0;
+
+            for(int i = 2; i <= n; i++)
+            {
+                bool bIsPrime = true;
+
+                for(int j = 2; j * j <= i; j++)
+                {
+                    if(i % j == 0)
+                    {
+                        bIsPrime = false;
+                        break;
+                    }
+                }
+
+                if(bIsPrime)
+                {
+                    answer++;
+                }
+            }
+
             return answer;
         }
     }
