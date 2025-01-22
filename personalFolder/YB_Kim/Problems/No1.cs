f40f85b423e4f6c9328685eee4f247a7a29e8653
kyb-coronatus <79882872+kyb-coronatus@users.noreply.github.com>
2025-01-22 21:26:41 +0900

    kyb 문제풀이 1
    콜라문제

--- personalFolder/YB_Kim/Problems/No1.cs
+++ personalFolder/YB_Kim/Problems/No1.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Threading.Channels;
+
 namespace YB_Kim.Problems
 {
 
@@ -7,8 +10,43 @@ namespace YB_Kim.Problems
         //https://school.programmers.co.kr/learn/courses/30/lessons/132267
         public int solution(int a, int b, int n)
         {
-            int answer = 0;
+            int answer = 0;     // Income 누적
+
+            int nInput = a;     // a개를 주면
+            int nOutput = b;    // b개 만큼 돌려줌
+            int nVariable = n;  // 투입량
+
+            int nExchange = 0;  // 교환량
+            int nChange = 0;    // 잔량
+
+            // a = 2, b = 1, c = 20
+
+            while(true)
+            {
+                nExchange = (int)(nVariable / nInput) * nOutput;
+                nChange = nVariable % nInput;
+
+                answer += nExchange;
+                nVariable = nExchange + nChange;
+
+                if(nChange >= nInput)
+                {
+                    nChange -= nInput;
+                    nVariable += nOutput;
+                }
+
+                if(nVariable == nInput)
+                {
+                    answer += nOutput;
+                    break;
+                }
+
+                //탈출조건
+                if(nVariable < nInput) break;
+            } 
+
             return answer;
         }
+
     }
 }
