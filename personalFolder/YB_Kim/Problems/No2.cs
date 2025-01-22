383cf557bf633bd63090026b6f67e825de827a74
kyb-coronatus <79882872+kyb-coronatus@users.noreply.github.com>
2025-01-22 23:05:57 +0900

    kyb 문제풀이 2

--- personalFolder/YB_Kim/Problems/No2.cs
+++ personalFolder/YB_Kim/Problems/No2.cs
@@ -1,13 +1,31 @@
+using System.Linq;
+
 namespace YB_Kim.Problems
 {
 
     public class No2
     {
-
         //https://school.programmers.co.kr/learn/courses/30/lessons/154539
         public int[] solution(int[] numbers)
         {
-            int[] answer = new int[] { };
+            int nLength = numbers.Length;
+            
+            int[] answer = new int[nLength];
+
+            List<int> listNumbers = numbers.ToList();
+
+            for(int i = 0; i < nLength; i++)
+            {
+                if(listNumbers[i] >= listNumbers.Skip(i).Max() || i == nLength - 1)
+                {
+                    answer[i] = -1;
+                }
+                else
+                {
+                    answer[i] = (from q in listNumbers.Skip(i) where q > listNumbers[i] select q).First();
+                }
+            }
+            
             return answer;
         }
     }
